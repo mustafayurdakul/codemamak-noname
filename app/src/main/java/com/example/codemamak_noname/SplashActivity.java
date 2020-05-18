462203e15be032e70ba9fdb3e27a8a7cd5b3358f
mustafa <yurdakulmustafa@outlook.com>
2020-05-18 21:38:51 +0300

    Harita düzetildi.

--- app/src/main/java/com/example/codemamak_noname/SplashActivity.java
+++ app/src/main/java/com/example/codemamak_noname/SplashActivity.java
@@ -24,8 +24,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 700);
-
-
+        }, 1000);
     }
 }

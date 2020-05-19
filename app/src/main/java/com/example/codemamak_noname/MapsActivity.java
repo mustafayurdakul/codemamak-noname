48403b2ef69a768a3ccf39603e5ac613b118ebca
mustafa <yurdakulmustafa@outlook.com>
2020-05-19 09:51:36 +0300

    Marker renkleri düzenlendi.

--- app/src/main/java/com/example/codemamak_noname/MapsActivity.java
+++ app/src/main/java/com/example/codemamak_noname/MapsActivity.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -51,9 +52,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng markerKutuphane = new LatLng(39.900369, 32.915591);
         LatLng markerDinlenPark = new LatLng(39.908608, 32.923790);
 
-        mMap.addMarker(new MarkerOptions().position(markerDemokPark).title("Demokrasi Parkı").snippet("Demokrasi parkındaki bankları seç!"));
-        mMap.addMarker(new MarkerOptions().position(markerKutuphane).title("Ege Mahallesi Halk Kütüphanesi").snippet("Kütüphanenin tabelası değişecek. Nasıl olsun?"));
-        mMap.addMarker(new MarkerOptions().position(markerDinlenPark).title("Dinlenme Parkı").snippet("En güzel çocuk parkını seçmemiz için bize yardım et!"));
+        mMap.addMarker(new MarkerOptions().position(markerDemokPark).title("Demokrasi Parkı").snippet("Demokrasi parkındaki bankları seç!").icon(BitmapDescriptorFactory.defaultMarker(45)));
+        mMap.addMarker(new MarkerOptions().position(markerKutuphane).title("Ege Mahallesi Halk Kütüphanesi").snippet("Kütüphanenin tabelası değişecek. Nasıl olsun?").icon(BitmapDescriptorFactory.defaultMarker(210)));
+        mMap.addMarker(new MarkerOptions().position(markerDinlenPark).title("Dinlenme Parkı").snippet("En güzel çocuk parkını seçmemiz için bize yardım et!").icon(BitmapDescriptorFactory.defaultMarker(10)));
 
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
